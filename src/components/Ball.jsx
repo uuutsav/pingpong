205dd4be477159d707f04a7dffb9435e8d75f2f6
uuutsav <21051903@kiit.ac.in>
2024-10-23 06:46:10 +0530

    Add basic ball logic

--- src/components/Ball.jsx
+++ src/components/Ball.jsx
@@ -1,11 +1,75 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
-const Ball = () => {
-  return (
-    <div className='h-5 w-5 bg-green-500 absolute '>
-        
-    </div>
-  )
+const Ball = ({ boardOffset }) => {
+    const startPosition = {
+        x: ((boardOffset.left * 2) + boardOffset.width - 12) / 2,
+        y: ((boardOffset.top * 2) + boardOffset.height - 12) / 2
+    };
+
+    const [position, setPosition] = useState(startPosition)
+    const ballRef = useRef();
+
+    let time = 40;
+    let speed = 5;
+
+    useEffect(() => {
+        // move ball
+        const intervalId = continueGame();
+        console.log("rendered")
+
+        return () => clearInterval(intervalId);
+    }, [])
+
+    const continueGame = () => {
+        const intervalId = setInterval(() => {
+            setPosition(prev => ({
+                x: prev.x + speed,
+                // y: prev.y + speed,
+                y: prev.y,
+            }));
+
+            const ballPosition = {
+                top: ballRef.current.offsetTop,
+                right: ballRef.current.offsetLeft + ballRef.current.offsetWidth,
+                bottom: ballRef.current.offsetTop + ballRef.current.offsetHeight,
+                left: ballRef.current.offsetLeft,
+            }
+
+            // red loss - ball touches left border of the board
+            if (ballPosition.left <= boardOffset.left) {
+                stopGame(intervalId);
+            }
+            // blue loss - ball touches right border of the board
+            if (ballPosition.right >= boardOffset.right) {
+                stopGame(intervalId);
+            }
+
+
+            if (position.x > (boardOffset.left + boardOffset.width)) {
+                console.log("ehe")
+            }
+        }, time)
+
+        return intervalId;
+    }
+
+    const stopGame = (intervalId) => {
+        clearInterval(intervalId)
+        setPosition(startPosition);
+    }
+
+    return (
+        <div className={`h-3 w-3 bg-green-200`}
+            style={{
+                position: `absolute`,
+                left: position.x,
+                top: position.y,
+            }}
+            ref={ballRef}
+        >
+
+        </div>
+    )
 }
 
 export default Ball
--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,18 +1,32 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Movables from './Movables'
 import Ball from './Ball'
 
 const Board = () => {
+    const pos = useRef();
+    const [boardOffset, setBoardOffset] = useState({})
+
+    useEffect(() => {
+        if (Object.keys(boardOffset).length == 0) {
+            setBoardOffset({
+                top: pos.current.offsetTop,
+                right: pos.current.offsetLeft + pos.current.offsetWidth,
+                bottom: pos.current.offsetTop + pos.current.offsetTop,
+                left: pos.current.offsetLeft,
+                height: pos.current.offsetHeight,
+                width: pos.current.offsetWidth,
+            })
+        }
+    }, [boardOffset])
+
     return (
-        <div className='h-[500px] w-[900px] bg-black flex flex-col justify-between'>
-            <div className="wall h-[11px] w-full bg-white">
+        <div className='h-[500px] w-[900px] bg-black flex flex-col justify-between' ref={pos}>
+            <div className="wall h-[11px] w-full bg-white">            </div>
 
-            </div>
             <Movables />
-            <Ball />
-            <div className="wall h-[11px] w-full mt-full bg-white">
+            {Object.keys(boardOffset).length > 0 && <Ball boardOffset={boardOffset} />}
 
-            </div>
+            <div className="wall h-[11px] w-full mt-full bg-white">            </div>
         </div>
     )
 }
